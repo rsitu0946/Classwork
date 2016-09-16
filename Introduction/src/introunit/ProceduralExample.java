20664afecadb12aebcd804eec8f65b4015e27847
Student6 <Student6@10.8.33.177>
2016-09-16 11:58:45 -0400

    Jillian says the year she is in.

--- Introduction/src/introunit/ProceduralExample.java
+++ Introduction/src/introunit/ProceduralExample.java
@@ -10,6 +10,8 @@ package introunit;
 public class ProceduralExample {
 	public static void main (String[] args){
 		String[] names = {"Jillian", "Joseph", "Jordan"};
+		String[] years = {"Senior", "Sophomore", "Junior"};
 		System.out.println("My name is "+ names[0] + ".");
+		System.out.println(names[0] + "is a " + years[0] + ".");
 	}
 }
