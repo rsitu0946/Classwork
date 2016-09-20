7108f7651bf747100cd26bdad6354d123c0ee791
Student6 <Student6@10.8.33.177>
2016-09-20 12:33:36 -0400

    Made a fair die roll

--- Introduction/src/datatypes/DataTypes.java
+++ Introduction/src/datatypes/DataTypes.java
@@ -10,7 +10,13 @@ public class DataTypes {
 	  is merely an integer.	*/
 	/*5*/	System.out.println(3+5.0/2+5*2);
 	/*6*/	System.out.println(3.0+5/2+5*2);
-	 
+	
+	if(42 == 42.0) System.out.println("42 equals 42.0");
+	
+	 String ten = "10";
+	 String two = "2";
+	 if(ten.compareTo(two)>0)System.out.println("10 is greater than two.");
+	 else System.out.println("10 is not greater than two.");
 	}
 
 }
