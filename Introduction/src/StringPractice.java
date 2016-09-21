28c298da0fe1fbf8d900af74f495d954ea27237f
Student6 <Student6@10.8.33.177>
2016-09-21 13:17:54 -0400

    Compared 2 strings

--- Introduction/src/StringPractice.java
+++ Introduction/src/StringPractice.java
@@ -2,7 +2,21 @@
 public class StringPractice {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+//		String text = new String("Hello World");
+		String text1 = "Hello World";//same as above
+		String text2 = "Hello ";
+		String text3 = "World";
+		
+		if(text1 == text2+text3){
+		System.out.println("These strings are equal.");
+		}			
+		System.out.println(text1);
+		System.out.println(text2+text3);
+		
+		String word1 = "Aardvark";
+		String word2 = "Zyzzyva";
+		if(word1.compareTo(word2) < 0)System.out.println("Word1 is before word2,"
+				+ " lexicographically.");
 
+	}
 }
