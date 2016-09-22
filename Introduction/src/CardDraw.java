fbe5fe8a03d17c08783539b610eb639a58791584
Student6 <Student6@10.8.33.177>
2016-09-22 12:34:14 -0400

    Ask for name input

---
+++ Introduction/src/CardDraw.java
@@ -0,0 +1,8 @@
+public class CardDraw {
+
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
--- Introduction/src/StringPractice.java
+++ Introduction/src/StringPractice.java
@@ -1,22 +1,66 @@
+import java.util.Scanner;
 
 public class StringPractice {
 
+	static Scanner input; 
+	static String user;
+	static int lineCount;
+	
 	public static void main(String[] args) {
-//		String text = new String("Hello World");
+		createAScanner();
+		lineCount = 0; 
+		demonstrateStringMethods();
+		talkForever();
+		promptName();
+	}
+	
+	private static void promptName(){
+		print("Hello, human! I am a board covered "
+				+ "with semiconductors \n " // \n makes a new line in console
+				+ "and other "
+				+ "such electronic components. "
+				+ "What is your name?");
+		user = input.nextLine();
+		print("Awesome! I will call you "+user+" until you terminate me.");
+	}
+
+	public static void talkForever(){
+		while(true){
+			promptInput();
+		}
+	}
+	public static void promptInput() {
+		print("Try inputting a String!");
+		String userInput = input.nextLine();
+		print("You typed: "+userInput);
+	}
+	
+	public static void print (String s){ 
+	//if you don't want to type System.out.println()
+		lineCount++;
+		System.out.println("Line #" +lineCount+": "+s);
+	}
+
+	public static void createAScanner() {
+		input = new Scanner(System.in);
+	}
+	
+	public static void demonstrateStringMethods(){
+		//String text = new String("Hello World");
 		String text1 = "Hello World";//same as above
 		String text2 = "Hello ";
 		String text3 = "World";
 		
 		if(text1 == text2+text3){
-		System.out.println("These strings are equal.");
+			System.out.println("These strings are equal.");
 		}			
 		System.out.println(text1);
 		System.out.println(text2+text3);
 		
 		String word1 = "Aardvark";
 		String word2 = "Zyzzyva";
+		
 		if(word1.compareTo(word2) < 0)System.out.println("Word1 is before word2,"
 				+ " lexicographically.");
-
 	}
 }
