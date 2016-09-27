c043ce395d4e830e53eb3ccd51775698cb0be441
Student6 <Student6@10.8.33.177>
2016-09-27 13:18:11 -0400

    Added noNegations

---
+++ Introduction/src/chatbot/RyanLike.java
@@ -0,0 +1,30 @@
+package chatbot;
+
+public class RyanLike implements Topic{
+	private boolean inLikeLoop;
+	private String likeResponse;
+	
+	public void talk(){
+		RyanMain.print("What are some things you like?");
+		inLikeLoop = true;
+		while(inLikeLoop){
+			likeResponse = RyanMain.getInput();
+			int likePsn = RyanMain.findKeyword(likeResponse, "like", 0);
+			if(likePsn >= 0){
+				String thingsLiked = likeResponse.substring(likePsn+5);
+				RyanMain.print("You are such an interesting person, because you like "+thingsLiked);
+				if(RyanMain.findKeyword(thingsLiked,"school",0) >= 0){
+					inLikeLoop = false;
+					RyanMain.school.talk();
+				}
+				inLikeLoop = false;
+				RyanMain.talkForever();
+			}	
+			else{
+				RyanMain.print("I don't understand you.");
+			}
+				
+			
+		}	
+	}
+}
--- Introduction/src/chatbot/RyanMain.java
+++ Introduction/src/chatbot/RyanMain.java
@@ -9,6 +9,7 @@ public class RyanMain {
 	static boolean inLoop;
 	static String response;
 	static Topic school;
+	static Topic like;
 	
 	public static void main(String[] args) {
 		createTopics();
@@ -22,45 +23,50 @@ public class RyanMain {
 	}
 	
 	public static void promptName(){
-		print("Hello, human! I am a board covered "
-				+ "with semiconductors \n " // \n makes a new line in console
-				+ "and other "
-				+ "such electronic components. "
-				+ "What is your name?");
+		System.out.println("Hello, human! I am a board covered with semiconductors \n and other such electronic components. What is your name?");
 		user = input.nextLine();
-		print("Awesome! I will call you "+user+" until you "
+		System.out.println("Awesome! I will call you "+user+" until you "
 				+ "terminate me.");
 	}
 
 	public static void talkForever(){
 		inLoop = true;
 		while(inLoop){
-			print("Greetings, "+user+" . How are you?");
+			System.out.println("Greetings, "+user+" . How are you?");
 			response = getInput();									
-			if(findKeyword(response, "good", 0)){
-				print("I'm so happy you're good.");
+			if(findKeyword(response, "good", 0) >= 0){
+				System.out.println("I'm so happy you're good. \n");
+			}
+			else if(findKeyword(response, "like", 0)) >= 0
+			{
+				
 			}
 			
-			else if(response.indexOf("school") >= 0)
+			else if(findKeyword(response,"school",0) >= 0)
 			{
 				inLoop = false; //exit this loop
 				school.talk();
 			}
 			else{
-				print("I'm sorry, I don't understand you.");
+				System.out.println("I'm sorry, I don't understand you. \n");
 			}
 		}
 	}
 	
-	public static boolean findKeyword(String searchString, String key, int startIndex) {
+	public static int findKeyword(String searchString, String key, int startIndex) {
 		//delete white space
 		String phrase = searchString.trim();
 		
 		//set all letters to lowercase
+		phrase=phrase.toLowerCase();
 		key = key.toLowerCase();
+		
+//		System.out.println("The phrase is " +phrase);
+//		System.out.println("The key is " +key);
 		
 		//find position of key
 		int psn = phrase.indexOf(key);
+//		System.out.println("The position found is "+psn);
 		
 		//keep looking for the word until you find the right context
 		while(psn >= 0){
@@ -69,30 +75,64 @@ public class RyanMain {
 			
 			//if the phrase does not end with this word 
 			if(psn + key.length() < phrase.length()){
-				after = phrase.substring(psn + key.length(), psn + key.length() + 1).toLowerCase();
+				after = phrase.substring(psn + key.length(),psn+key.length()+1).toLowerCase();
+//				System.out.println("The character after "+key+"is "+after);
 			}
 
 			//if the phrase does not begin with this word
 			if(psn >0){
 					before = phrase.substring(psn-1,psn).toLowerCase();
+//					System.out.println("The character before "+key +" is "+before);
 			}
 			
 			if(before.compareTo("a") < 0 && after.compareTo("a") < 0){
-				return true;
+//				System.out.println(key+" was found at "+psn);
+				if(noNegations(phrase,psn)){
+					return psn;
+				}
 			}
+			
 			//in case the keyword was not found yet,
 			//check the rest of the string
 			psn = phrase.indexOf(key,psn+1);
+//			System.out.println(key+" was not found. Checking "+psn);
 		}
 		
-		return false;
+		return -1;
 	}
-
-	public static void promptInput() {
-		print("Try inputting a String!");
-		String userInput = input.nextLine();
-		print("You typed: "+userInput);
+	
+	//"helper method" - a method that contributes 
+	//functionality 
+	//to another method. Helper methods are very common when you need to do the same thing repeatedly. They also help make methods more readable.
+	//This method is private because it is only used by the method it is helping.
+	
+	private static boolean noNegations(String phrase, int index){
+		//check for word "NO "; (3 characters (including space))
+		//check to see if there is space for the word
+		//"NO " to be in front of the index
+		if(index - 3 >=0 && phrase.substring(index-3,index).equals("no ")){
+			return false;
+		}
+		//check for "not "
+		if(index - 4 >= 0 && phrase.substring(index-3,index).equals("not ")){
+			return false;
+		}
+		//check for never
+		if(index - 6 >=0 && phrase.substring(index-3,index).equals("never ")){
+			return false;
+		}
+		//check for "n't"
+		if(index - 4 >=0 && phrase.substring(index-3,index).equals("n't ")){
+			return false;
+		}
+		return true;
 	}
+	
+//	public static void promptInput() {
+//		print("Try inputting a String!");
+//		String userInput = input.nextLine();
+//		print("You typed: "+userInput);
+//	}
 	
 	public static String getInput(){
 		return input.nextLine();
@@ -128,11 +168,13 @@ public class RyanMain {
 			}
 			printString += currentLine = "\n";
 		}
+		
 		System.out.println(printString);
 	}
 
 	public static void createTopics() {
 		input = new Scanner(System.in);
 		school = new School();
+		like = new RyanLike();
 	}	
 }
