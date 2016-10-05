969409c479980d792a63661b7299e3d72f972f0f
Student6 <Student6@10.8.33.177>
2016-10-05 13:18:05 -0400

    added Clinton

--- Introduction/src/chatbot/RyanHello.java
+++ Introduction/src/chatbot/RyanHello.java
@@ -1,60 +1,59 @@
 package chatbot;
 
 public class RyanHello implements Topic {
-	
+
 	private boolean inHelloLoop;
 	private String helloResponse;
-	
+
 	private int helloCount;
-	
-	//responses are constants (never change, always exist)
-	private static String[] calmResponses = {"We've already said our hellos. Remember?", "Yes, hello to you too. Let's "
-			+ "actually talk."};
-	
-	private static String[] angryResponses = {"Okay, seriously. This has to stop.", "You are beginning to annoy me. "
-			+ "We've SAID HELLO!"
-	};
-	
-	public RyanHello(){
+
+	// responses are constants (never change, always exist)
+	private static String[] calmResponses = { "We've already said our hellos. Remember?",
+			"Yes, hello to you too. Let's " + "actually talk." };
+
+	private static String[] angryResponses = { "Okay, seriously. This has to stop.",
+			"You are beginning to annoy me. " + "We've SAID HELLO!" };
+
+	public RyanHello() {
 		helloCount = 0;
 	}
-	
+
 	public void talk() {
 		inHelloLoop = true;
-		while(inHelloLoop){
+		while (inHelloLoop) {
 			helloCount++;
-			printResponse(); 
+			printResponse();
 			helloResponse = RyanMain.getInput();
-			//! is used for negation
-			if(!isTriggered(helloResponse)){
+			// ! is used for negation
+			if (!isTriggered(helloResponse)) {
 				inHelloLoop = false;
 				RyanMain.talkForever();
 			}
 		}
-		
+
 	}
 
-	private void printResponse(){
+	private void printResponse() {
 		int responseIndex = 0;
-		//calm response
-		if(helloCount < 5){
-			responseIndex = (int)(Math.random() * calmResponses.length);
+		// calm response
+		if (helloCount < 5) {
+			responseIndex = (int) (Math.random() * calmResponses.length);
 			RyanMain.print(calmResponses[responseIndex]);
 		}
-		//angry response
-		else{
-			responseIndex = (int)(Math.random() * angryResponses.length);
+		// angry response
+		else {
+			responseIndex = (int) (Math.random() * angryResponses.length);
 			RyanMain.print(angryResponses[responseIndex]);
-			
+
 		}
-		
+
 	}
 
 	public boolean isTriggered(String userInput) {
-		if(RyanMain.findKeyword(userInput, "hi", 0) >= 0){
+		if (RyanMain.findKeyword(userInput, "hi", 0) >= 0) {
 			return true;
 		}
-		if(RyanMain.findKeyword(userInput, "hello", 0) >= 0){
+		if (RyanMain.findKeyword(userInput, "hello", 0) >= 0) {
 			return true;
 		}
 		return false;
--- Introduction/src/chatbot/RyanLike.java
+++ Introduction/src/chatbot/RyanLike.java
@@ -1,32 +1,32 @@
 package chatbot;
 
-public class RyanLike implements Topic{
+public class RyanLike implements Topic {
 	private boolean inLikeLoop;
 	private String likeResponse;
-	
-	public void talk(){
+
+	public void talk() {
 		RyanMain.print("What are some things you like?");
 		inLikeLoop = true;
-		while(inLikeLoop){
+		while (inLikeLoop) {
 			likeResponse = RyanMain.getInput();
 			int likePsn = RyanMain.findKeyword(likeResponse, "like", 0);
-			if(likePsn >= 0){
-				String thingsLiked = likeResponse.substring(likePsn+5);
-				RyanMain.print("You are such an interesting person, because you like "+thingsLiked);
-				if(RyanMain.findKeyword(thingsLiked,"school",0) >= 0){
+			if (likePsn >= 0) {
+				String thingsLiked = likeResponse.substring(likePsn + 5);
+				RyanMain.print("You are such an interesting person, because you like " + thingsLiked);
+				if (RyanMain.findKeyword(thingsLiked, "school", 0) >= 0) {
 					inLikeLoop = false;
 					RyanMain.school.talk();
 				}
 				inLikeLoop = false;
 				RyanMain.talkForever();
-			}	
-			else{
+			} else {
 				RyanMain.print("I don't understand you.");
 			}
-		}	
+		}
 	}
+
 	public boolean isTriggered(String userInput) {
-		if(RyanMain.findKeyword(userInput, "like", 0) >= 0){
+		if (RyanMain.findKeyword(userInput, "like", 0) >= 0) {
 			return true;
 		}
 		return false;
