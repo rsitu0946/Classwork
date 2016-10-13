184f024f89d095873bf76a9e66fde6ed9d57be74
Student6 <Student6@10.8.33.177>
2016-10-13 13:13:11 -0400

    changeString

--- Introduction/src/arrays/ArraysMain.java
+++ Introduction/src/arrays/ArraysMain.java
@@ -5,13 +5,36 @@ public class ArraysMain {
 	public static void main(String[] args) {
 		//This is how you time how quickly a computer processes.
 		long startTime = System.currentTimeMillis();
-		arrayIntroMethod();
+		String[] someStrings = new String[100];
+		populateArray(someStrings);
+		
+		changeString(someStrings[99]);
+		
+		printArray(someStrings);
 		long endTime = System.currentTimeMillis();
 		System.out.println("Completed method in "
 				+ (endTime-startTime)+ " milliseconds");	
 		}
 	
-	public static void arrayIntroMethod(){
+	private static void changeString(String s) {
+		s = "This string has been changed!";
+	}
+
+	private static void printArray(String[] a) {
+		//this loop prints the strings
+		for(String s: a){
+			System.out.println(s);
+		}
+	}
+
+	private static void populateArray(String[] a) {
+		//this loop instantiating the strings
+		for(int index1 = 0; index1 < a.length; index1++){
+			a[index1] = "value "+(index1+1);
+		}
+	}
+
+	public static void arrayIntroMethod(){	//broken down into 2 methods
 		//construct 3 integer arrays
 				//these two methods do the same thing
 				//primitive type arrays fill with ZEROES 0, 0.0, false ,
@@ -35,15 +58,17 @@ public class ArraysMain {
 				for(boolean b: booleans){
 					System.out.println(""+b);
 				}
-				
-				
+							
 				//these two constructors are different
 				String[] strings1 = {"","",""};
 				String[] strings2 = new String[3];
 				
-				for(String s: strings1){
-					System.out.println(s);
+				//this loop instantiating the strings
+				for(int index1 = 0; index1 < strings2.length; index1++){
+					strings2[index1] = "value "+(index1+1);
 				}
+				
+				//this loop prints the strings
 				for(String s: strings2){
 					System.out.println(s);
 				}
